733d448821f1ca135f25f68b65830b62059d2d79
luanaFOliveira <luluoliveira7@gmail.com>
2023-10-22 19:08:16 -0300

    livro controller and api routes

---
+++ app/Http/Controllers/LivrosController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Livro;
+use Illuminate\Http\Request;
+
+class LivrosController extends Controller
+{
+    public function index()
+    {
+        return Livro::all();
+    }
+
+    public function show(Livro $livro)
+    {
+        return $livro;
+    }
+
+    public function store(Request $request)
+    {
+        $livro = Livro::create($request->all());
+        return response()->json($livro,201);
+    }
+
+    public function update(Request $request,Livro $livro)
+    {
+        $livro->update($request->all());
+        return response()->json($livro,200);
+    }
+
+    public function destroy(Livro $livro)
+    {
+        $livro->delete();
+        return response()->json(null,204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\LivrosController;
+
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('livros', [LivrosController::class, 'index']);
+Route::get('livros/{livro}', [LivrosController::class, 'show']);
+Route::post('livros', [LivrosController::class, 'store']);
+Route::put('livros/{livro}', [LivrosController::class, 'update']);
+Route::delete('livros/{livro}', [LivrosController::class, 'destroy']);
+
