83b131b7e2e8031aa2d2a5ae9a6252ab99d06f4a
luanaFOliveira <luluoliveira7@gmail.com>
2023-10-27 14:38:31 -0300

    fixing error

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './NavBar.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 //        <a className="navbar-brand" href="/">Biblioteca Pessoal</a>
 //a className="nav-item nav-link" href="/listBooks">Meus Livros</a>
