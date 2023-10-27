d45be08dafc3a71fb6f161719073bca180c365b6
luanaFOliveira <luluoliveira7@gmail.com>
2023-10-27 13:49:15 -0300

    changing db configs

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'bibliotecapessoal'),
             'username' => env('DB_USERNAME', 'luanafoliveira'),
